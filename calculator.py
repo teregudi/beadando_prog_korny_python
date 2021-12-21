a4ca3a4ac61ad389fa873c7d0729e71e58c93076
teregudi <teregudi.tamashi@gmail.com>
2021-12-21 12:35:48 +0100

    wiring the buttons

--- calculator.py
+++ calculator.py
@@ -7,7 +7,29 @@ Builder.load_file('./calculator.kv')
 Window.size = (350, 550)
 
 class CalculatorWidget(Widget):
-    pass
+    def clear(self):
+        self.ids.input_box.text = '0'
+
+    def button_value(self, number):
+        prev_number = self.ids.input_box.text
+        if prev_number == '0':
+            self.ids.input_box.text = ''
+            self.ids.input_box.text = f"{number}"
+        else:
+            self.ids.input_box.text = f"{prev_number}{number}"
+
+    def signs(self, sign):
+        prev_number = self.ids.input_box.text
+        self.ids.input_box.text = f"{prev_number}{sign}"
+
+    def remove_last(self):
+        prev_number = self.ids.input_box.text
+        prev_number = prev_number[:-1]
+        self.ids.input_box.text = f"{prev_number}"
+
+    def result(self):
+        
+
 
 class CalculatorApp(App):
     def build(self):
